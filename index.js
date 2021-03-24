8f71d4336ddaef0687ac99bf44bd5a5903c3bf5a
BrennoMelo <brennoprogmm@gmail.com>
2021-03-24 10:27:04 -0300

    Finalização do Projeto

--- index.js
+++ index.js
@@ -17,10 +17,37 @@ for(person in data){
     tableBody.appendChild(newRow)
 
     personData = data[person];
+    
+    _count = 0;
     for(info of arrayData){
+        _count +=1;
+
         const newColumn = document.createElement('td')
         newColumn.setAttribute("scope", "col")
-        newColumn.innerText = info
+        newColumn.innerText = personData[info]
+        objPersonData[info] = personData[info]
         newRow.appendChild(newColumn);
+
+        if(_count == arrayData.length){
+            const newButton = document.createElement("button")
+            newButton.setAttribute("class", "btn btn-primary btn-sm")
+            newButton.innerText = "Detalhes"
+
+            const newColumn = document.createElement('td')
+            newColumn.setAttribute("scope", "col")
+            newColumn.appendChild(newButton)
+            newRow.appendChild(newColumn)
+
+            let alertText = "";
+            for(attribute in objPersonData){
+                alertText+= `\r\n ${attribute} = ${objPersonData[attribute]}`
+            }
+
+            newButton.addEventListener("click", () => {
+                alert(alertText)
+            })
+        }
+    
     }
+    
 }
